a25b4ad359c0a57968ba20012e4bd439ad0db5e9
kelvin.sousa <kelvin.rodrigues@tinnova.com.br>
2025-11-16 17:47:25 -0300

    Fix config

--- backend/config/config.go
+++ backend/config/config.go
@@ -21,6 +21,29 @@ type Diino struct {
 	Service     *service.Service
 }
 
+package config
+
+import (
+	"log"
+	"orcamental/backend/internal/controllers"
+	"orcamental/backend/internal/repository"
+	"orcamental/backend/internal/service"
+	"orcamental/backend/pkg/auth"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"gorm.io/gorm"
+)
+
+type Diino struct {
+	DB          *gorm.DB
+	Routes      *gin.Engine
+	Auth        *auth.Auth
+	Controllers *controllers.Controllers
+	Repository  *repository.Repository
+	Service     *service.Service
+}
+
 func Init(r *gin.Engine) (diino *Diino, err error) {
 	defer func() {
 		if r := recover(); r != nil {
@@ -32,21 +55,27 @@ func Init(r *gin.Engine) (diino *Diino, err error) {
 	}
 
 	d := &Diino{}
-	d.ConnectDB()
+	if err = d.ConnectDB(); err != nil {
+		log.Fatal("⚠️  Não foi possivel inicializar sistemas de repository")
+		return
+	}
 
 	d.Repository, err = repository.RegisterRepository(d.DB)
 	if err != nil {
 		log.Fatal("⚠️  Não foi possivel inicializar sistemas de repository")
+		return
 	}
 
 	d.Auth, err = auth.SetAuth(d.Repository)
 	if err != nil {
 		log.Fatal("⚠️  Não foi possivel inicializar sistemas de auth")
+		return
 	}
 
 	d.Service, err = service.RegisterService(d.Repository)
 	if err != nil {
 		log.Fatal("⚠️  Não foi possivel inicializar sistemas de services")
+		return
 	}
 
 	return d, err
